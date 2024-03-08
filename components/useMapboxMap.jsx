535551414aa5e25e25afa08b16a0e1156b930591
cbeetler <71681083+cbeetler@users.noreply.github.com>
2024-03-08 12:15:26 -0500

    Removed any non-JSX code related to the mapbox-gl from index.jsx. Created a custom hook in a new component in accordance with iteration 11, useMapboxMap.jsx. This build of the website never displays or encounters the map but it seemed like a good idea to make sure it's in accordance with the project's slight change of structure moving forward.

---
+++ components/useMapboxMap.jsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import mapboxgl from "mapbox-gl/dist/mapbox-gl.js";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+export default function useMapboxMap(container) {
+  const [marker, setMarker] = useState();
+
+  // Mapbox token
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoidnNscWRzaGNiaWlnc291eGtkIiwiYSI6ImNsM3dyZDhvaTBucjkzbGxkM2syYTZpYW4ifQ.UyVIpJDgWM2-ZIufnxMBZQ";
+
+  // renders map once
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: container,
+      style: "mapbox://styles/mapbox/dark-v10", // sets the dark mode
+      center: [12.567898, 55.67583], // Sets the center of the map (long, lat)
+      zoom: 9,
+    });
+    // create marker
+    /*
+    const mapMarker = new mapboxgl.Marker()
+      .setLngLat([12.567898, 55.67583])
+      .addTo(map); // add it to the existing map
+    setMarker(mapMarker); // update state of marker to default location
+    */
+  }, []);
+
+  // store coordinates (related to map)
+  const stores = {
+    central: [12.567898, 55.67583],
+    norrebro: [12.553806, 55.699299],
+    airport: [12.650784, 55.618042],
+  };
+
+  // since useEffect only renders once, this will update the mapMarker set there
+  function handleLocationChange(e) {
+    const location = e.target.value;
+    marker.setLngLat(stores[location]);
+  }
+}
--- index.jsx
+++ index.jsx
@@ -9,59 +9,28 @@ import mapboxgl from "mapbox-gl/dist/mapbox-gl.js";
 
 // components
 import StoreFront from "./components/StoreFront.jsx";
-// import Product from "./components/Product.jsx";
+// import useMapBoxMap from "./components/useMapboxMap.jsx";
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
-  // const [random, setRandom] = useState(Math.random()); // previously used with map
-  const [marker, setMarker] = useState();
 
   return <StoreFront />;
 
-  // everything else in <App /> will not run
+  /* the code below is related entirely to the map
 
-  // Mapbox token
-  mapboxgl.accessToken =
-    "pk.eyJ1IjoidnNscWRzaGNiaWlnc291eGtkIiwiYSI6ImNsM3dyZDhvaTBucjkzbGxkM2syYTZpYW4ifQ.UyVIpJDgWM2-ZIufnxMBZQ";
-
-  // renders map once
-  useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: "map", // matches <div id="map" />
-      style: "mapbox://styles/mapbox/dark-v10", // sets the dark mode
-      center: [12.567898, 55.67583], // Sets the center of the map (long, lat)
-      zoom: 9,
-    });
-    // create marker
-    const mapMarker = new mapboxgl.Marker()
-      .setLngLat([12.567898, 55.67583])
-      .addTo(map); // add it to the existing map
-    setMarker(mapMarker); // update state of marker to default location
-  }, []);
-
-  // store coordinates (related to map)
-  const stores = {
-    central: [12.567898, 55.67583],
-    norrebro: [12.553806, 55.699299],
-    airport: [12.650784, 55.618042],
-  };
-
-  // since useEffect only renders once, this will update the mapMarker set there
-  function handleLocationChange(e) {
-    const location = e.target.value;
-    marker.setLngLat(stores[location]);
-  }
-
+  useMapboxMap("map");
+  
   return (
     <>
-      {/* <button
+      // button was commented out prior to updating
+      {<button
         id="re-render"
         // button hidden without below style attributes
         style={{ zIndex: "10", position: "absolute" }}
         onClick={() => setRandom(Math.random())}
       >
         Re-render
-      </button> */}
+      </button>}
       <div className="map-overlay">
         <h3>Choose store: </h3>
         <select onChange={handleLocationChange}>
@@ -70,7 +39,7 @@ function App() {
           <option value="airport">CPH Airport</option>
         </select>
       </div>
-      {/* for the div, class "mapboxgl-canvas" was necessary in order to work with .css */}
+      {// for the div, class "mapboxgl-canvas" was necessary in order to work with .css}
       <div id="map" className="mapboxgl-canvas"></div>
     </>
   );
